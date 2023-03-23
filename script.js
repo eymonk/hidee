55f60d5d9972ec78dd072181a78ef2973b939cb0
JaySun0 <jaysun0@yandex.ru>
2023-03-23 14:00:26 +0300

    reafactor: encryptor function

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 (function () {
   const symbols = Array.from(new Set('tч !0"щэюя#)./1CDdЙO4G£шHI69:>ъь?}RS[\\ВГ^_`~@AеBL&\'(lСMцNPЕ5ЁЖЗQTUpqrVлWдeжXbmуфYZ*о+aйcfgCкDhijkРnЬЭЮosuvEFwxyzАМБДИ{|КЛ2бв3НОПТУ$ы%ФХЦ]ЧШ;<=ЩЪЫЯагзи7ё8мнJKрп,-стх'));
   const lastIndex = symbols.length - 1;
-  const counterRange = 100;
-  let notificationTimeout;
   const dom = {
     input: document.querySelector('.encryptor__input'),
     notification: document.querySelector('.encryptor__notification'),
@@ -12,19 +10,22 @@
     clearBtn: document.querySelector('.encryptor__button_clear'),
   };
 
+
   function getRandomInRange(rangeStart, rangeEnd) {
     return Math.round(Math.random() * (rangeEnd - rangeStart)) + rangeStart;
   }
 
+
+  let notificationTimeout = null;
   function notify(message) {
     if (notificationTimeout) clearTimeout(notificationTimeout);
     dom.notification.innerText = message;
     dom.notification.style.color = getComputedStyle(document.documentElement).getPropertyValue('--accent-color');
     notificationTimeout = setTimeout(() => {
-      dom.notification.style.color = 'transparent';
-      dom.notification.innerText = 'ama hidden)))';
+      dom.notification.innerText = '';
     }, 1800);
   }
+
 
   function copy() {
     const text = dom.input.value;
@@ -33,22 +34,48 @@
     notify('copied');
   }
 
+
   function clear() {
     dom.input.value = '';
     dom.input.focus();
     notify('cleared');
   }
 
+
   function changeCounter(counter, step) {
+    const counterRange = 100;
+
     counter += step;
     if (counter > counterRange) counter -= counterRange;
+
     return counter;
   }
+
+
+  function reindex(inputSymbols, counter, step) {
+    const reindexedSymbols = [];
+
+    function findNewIndex(symbolIndex) {
+      let newIndex;
+      counter = changeCounter(counter, step);
+      if (symbolIndex + counter > lastIndex) newIndex = (symbolIndex + counter) - lastIndex - 1;
+      else newIndex = symbolIndex + counter;
+      return newIndex;
+    }
+
+    for (let i = 0; i < inputSymbols.length; i++) {
+      if (inputSymbols[i] === '\n') inputSymbols[i] = ' ';
+      const newIndex = findNewIndex(symbols.indexOf(inputSymbols[i]));
+      reindexedSymbols.push(symbols[newIndex]);
+    }
+
+    return reindexedSymbols;
+  }
+
 
   function encrypt() {
     if (dom.input.value) {
       const inputSymbols = dom.input.value.split('');
-      const newSymbols = [];
       const step = getRandomInRange(2, 8);
       const counterInitialValue = getRandomInRange(1, 8);
       const deceiveSymbols = [];
@@ -60,20 +87,7 @@
         deceiveSymbols.push(symbols[randomSymbolIndex]);
       };
 
-      function findNewIndex(symbolIndex, step) {
-        let newIndex;
-        counter = changeCounter(counter, step);
-        if (symbolIndex + counter > lastIndex) newIndex = (symbolIndex + counter) - lastIndex - 1;
-        else newIndex = symbolIndex + counter;
-        return newIndex;
-      }
-
-      for (let i = 0; i < inputSymbols.length; i++) {
-        if (inputSymbols[i] === '\n') inputSymbols[i] = ' ';
-        const newIndex = findNewIndex(symbols.indexOf(inputSymbols[i]), step);
-        newSymbols.push(symbols[newIndex]);
-      }
-
+      const newSymbols = reindex(inputSymbols, counter, step);
       let encipheredText = newSymbols.join('');
       for (let i = deceiveSymbolsLength - 1; i > 0; i--) encipheredText = `${encipheredText}${deceiveSymbols[i]}`;
       encipheredText = `${encipheredText}${counterInitialValue}${step}${deceiveSymbolsLength}`
@@ -82,6 +96,7 @@
       notify('encrypted');
     } else notify('nothing to encrypt');
   }
+
 
   function decrypt() {
     if (dom.input.value) {
@@ -105,8 +120,10 @@
     } else notify('nothing to decrypt');
   }
 
+
   dom.encryptBtn.addEventListener('click', encrypt);
   dom.decryptBtn.addEventListener('click', decrypt);
   dom.copyBtn.addEventListener('click', copy);
   dom.clearBtn.addEventListener('click', clear);
+  notify('qq)');
 }());
